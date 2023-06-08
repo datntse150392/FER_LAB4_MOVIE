9d5c98685f15e63b98695b34cd45a179c87811ce
Nguyen Thanh Dat <thuykhangvn@gmail.com>
2023-06-08 17:26:30 +0700

    update responsvie

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -21,7 +21,7 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: "60%",
+  width: "70%",
   bgcolor: "background.paper",
   borderradius: "20px",
   boxShadow: 24,
